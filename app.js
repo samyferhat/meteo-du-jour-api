8576226c3e689a94cd3aa67f2f0c28fe92278a04
samy <udev9985@gmail.com>
2023-12-09 21:59:26 +0100

    second commit

--- app.js
+++ app.js
@@ -10,37 +10,38 @@ async function getMeteoOfDay() {
         const datei = new Date(meteoOfDay.hourly.time[1])
         console.log(datei.getHours())
         domUpdate(meteoOfDay);
-        
-    } catch(error) {
+
+    } catch (error) {
         console.log(error);
     }
-    
-    
-} 
+
+
+}
 
 
 // getHoursOfDay retourne un tableau avec des objets heure, chaque objet 
 // heure a trois proprites : {time, weather_code, is_current} 
 // ex : [ {time: (date), weather_code: "0n",is_current:1}]
 
-const getHoursOfDay = ({hourly,current}) => {
+const getHoursOfDay = ({ hourly, current }) => {
 
     let tab = [];
 
     // is_day_code est une fonction qui retourne n pour nuit et d pour jour .
-    const is_day_code = (is_day) => is_day == 0 ? "n":"d";
+    const is_day_code = (is_day) => is_day == 0 ? "n" : "d";
 
     // is_current_hour est une fonction qui retourne 1 si l'heure de la
     // date donnée en parametre est l'heure actuelle et 0 sinon .
-    const is_current_hour = (date) => date.getHours() == (new Date(current.time)).getHours() ? 1:0 ;
+    const is_current_hour = (date) => date.getHours() == (new Date(current.time)).getHours() ? 1 : 0;
 
-    for (let i = 0 ; i < 24; ++i) {
+    for (let i = 0; i < 24; ++i) {
         const h = new Date(hourly.time[i]);
-        tab.push({time: h, is_day: hourly.is_day[i],
-             weather_code: `${hourly.weather_code[i]}` 
-             + is_day_code(hourly.is_day[i]),
-            is_current: is_current_hour(h)});
-        console.log(is_current_hour(h))
+        tab.push({
+            time: h, is_day: hourly.is_day[i],
+            weather_code: `${hourly.weather_code[i]}`
+                + is_day_code(hourly.is_day[i]),
+            is_current: is_current_hour(h)
+        });
     }
     return tab;
 }
@@ -52,18 +53,17 @@ const domUpdate = (meteoOfDay) => {
     const hoursOfDay = getHoursOfDay(meteoOfDay);
 
     const ul = document.querySelector(".gridTab");
-    console.log(ul);
     ul.insertAdjacentHTML(
-    "afterBegin",
-    hoursOfDay.reduce(
-        (acc, {time, weather_code, is_current}) => acc 
-        + `<li data-time="${time}" 
-        ${is_current == 1 ? "class=\"current-h\"":""}>
+        "afterBegin",
+        hoursOfDay.reduce(
+            (acc, { time, weather_code, is_current }) => acc
+                + `<li data-time="${time}" 
+        ${is_current == 1 ? "class=\"current-h\"" : ""}>
         <img src="icons/${weather_code}.png"><div class="note">
         <p >${time.getHours()}</p></div></li>`,
-        ""
-    )
-);
+            ""
+        )
+    );
 }
 
 
